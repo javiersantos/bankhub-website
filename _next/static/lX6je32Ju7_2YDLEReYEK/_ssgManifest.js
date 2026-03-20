8f14094cf7f78cb7230abcd05f9d14ea78cb9c4d
Javier Santos <me@javiersantos.me>
2026-03-20 16:55:54 +0100

    Release new update

---
+++ _next/static/lX6je32Ju7_2YDLEReYEK/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[lang]","\u002F[lang]\u002Fdownload","\u002F[lang]\u002Fexplore\u002F[id]","\u002F[lang]\u002Fprivacy","\u002F[lang]\u002Fshare","\u002F[lang]\u002Fterms","\u002Fexplore\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
